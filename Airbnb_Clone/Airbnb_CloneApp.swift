ce5ceabd02f592592794ef530234ff732826e4fe
iMakca <ukr.maksim1@gmail.com>
2023-10-21 10:11:00 +0300

    create project

---
+++ Airbnb_Clone/Airbnb_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Airbnb_CloneApp.swift
+//  Airbnb_Clone
+//
+//  Created by Maxim Hranchenko on 20.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct Airbnb_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
